bf30160e8cc2f18503a42af4c4e03a58ae6f312a
Jamie Duddleston <guildedthorn@gmail.com>
2022-06-02 19:27:10 -0500

    Binary API Update

--- serializer/PacketSerializer.cs
+++ serializer/PacketSerializer.cs
@@ -1,9 +1,13 @@
-﻿using SkyWing.Binary;
+﻿using System.Text;
+using SkyWing.Binary;
 
 namespace BedrockProtocol.Serializer;
 
-public class PacketSerializer : BinaryStream{
+public class PacketSerializer : BinaryStream {
+	public PacketSerializer(int size) : base(size) {}
 
-	public PacketSerializer(int size) : base(size) {
-	}
+    public void WriteString(string value) {
+        WriteUnsignedVarInt(UnSignInt(value.Length));
+        WriteBytes(Encoding.UTF8.GetBytes(value));
+    }
 }
